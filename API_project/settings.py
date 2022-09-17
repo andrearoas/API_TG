022ab61cfacea12000ea49047935ce7ca2aba6d1
Paola Roa <paola.roa@correounivalle.edu.co>
2022-09-16 20:03:06 -0500

    commit 1.4.2 changed DEBUG  in settings.py

--- API_project/settings.py
+++ API_project/settings.py
@@ -21,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-phjv)fwj)&20t-3e(r2qepnj_(ay%!$q18bqn)x-9b$)uzd6&*'
+# SECRET_KEY = 'django-insecure-phjv)fwj)&20t-3e(r2qepnj_(ay%!$q18bqn)x-9b$)uzd6&*'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-phjv)fwj)&20t-3e(r2qepnj_(ay%!$q18bqn)x-9b$)uzd6&*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = [
     '*',
